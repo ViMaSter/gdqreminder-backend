e3c46a1260e94d20444158a25d748062bfe644ec
Vincent Mahnke <vincent@mahn.ke>
2025-05-19 04:12:30 +0200

    chore: Removes empty test file

--- __tests__/store/dataContainer/schedule.test.js
+++
@@ -1,9 +0,0 @@
-import { FakeTimeProvider } from "../../../services/timeProvider/fakeTimeProvider";
-import { FakeHTTPClient } from "../../stubs/fakeHTTPClient";
-import { DataContainer } from "../../../store/dataContainer"
-
-describe("dataContainer", () => {
-    describe("schedule", () => {
-        it("updates if a new schedule is released and has runs", () => {})
-    });
-});
