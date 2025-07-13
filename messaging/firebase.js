9debb63c1d56159793cbb7b0e1dd83e86d9beceb
Vincent Mahnke <accounts@vimaster.de>
2025-07-13 00:30:56 +0000

    feat: Sends notifications when a new run is added to the schedule

--- messaging/firebase.js
+++ messaging/firebase.js
@@ -28,13 +28,44 @@ export default class Firebase {
 
         let logData = {firebaseMessage, id: run.id, name: run.name, reason};
 
-        this.#logger.info("[FIREBASE] Sending message for run {id} ({name})... (Reason: {reason})", logData);
+        this.#logger.info("[FIREBASE] Sending start message for run {id} ({name})... (Reason: {reason})", logData);
         getMessaging().send(firebaseMessage)
             .then((response) => {
                 this.#logger.info("[FIREBASE] Successfully sent message for run {id} ({name}): {response} (Reason: {reason})", {...logData, response});
             })
             .catch((error) => {
                 this.#logger.info("[FIREBASE] Error sending message for run {id} ({name}): {error} (Error: {error})", {...logData, error});
+            });
+    }
+    sendNewlyAddedForRun(run, reason) {
+        const topic = `run.added`;
+
+        const firebaseMessage = {
+            notification: {
+                title: `GDQ Reminder: ${run.display_name}`,
+                body: "Just announced! Check the schedule!"
+            },
+            android: {
+                notification: {
+                    color: "#00aeef",
+                    icon: 'notification'
+                }
+            },
+            data: {
+                event: topic
+            },
+            topic: topic
+        };
+
+        let logData = {firebaseMessage, id: run.id, display_name: run.display_name, reason};
+
+        this.#logger.info("[FIREBASE] Sending run announcement for run {id} ({display_name})...", logData);
+        getMessaging().send(firebaseMessage)
+            .then((response) => {
+                this.#logger.info("[FIREBASE] Successfully sent message for run {id} ({display_name})", {...logData, response});
+            })
+            .catch((error) => {
+                this.#logger.info("[FIREBASE] Error sending message for run {id} ({display_name}): {error} (Error: {error})", {...logData, error});
             });
     }
     sendStartMessageForNewSchedule(event) {
